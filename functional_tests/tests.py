07fa0c032789ba29c827f1a208abea9f3fe66983
valdeiralmeida2025 <valdeir2012almeida@gmail.com>
2026-09-14 15:27:42 -0300

    Parte 1: Isolamento dos testes funcionais com LiveServerTestCase

---
+++ functional_tests/tests.py
@@ -0,0 +1,60 @@
+import time
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouviu falar de uma nova aplicação online interessante.
+        # Ela vai conferir sua página inicial
+        self.browser.get(self.live_server_url)
+
+        # Ela nota que o título da página e o cabeçalho mencionam listas de tarefas (To-Do)
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
+
+        # Ela é convidada a inserir um item de tarefa imediatamente
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"), "Enter a to-do item"
+        )
+
+        # Ela digita "Buy peacock feathers" em uma caixa de texto
+        inputbox.send_keys("Buy peacock feathers")
+
+        # Quando ela aperta Enter, a página é atualizada e agora a página lista
+        # "1: Buy peacock feathers" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        # Ainda há uma caixa de texto convidando-a a adicionar outro item.
+        # Ela insere "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # A página é atualizada novamente e agora mostra ambos os itens na lista
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table(
+            "2: Use peacock feathers to make a fly"
+        )
+
+        # Fim do teste por enquanto
